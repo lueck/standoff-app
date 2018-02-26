a228638e2a049026caeead12b1f99d03151d6c67
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-02-26 21:54:07 +0100

    Bibliography input. First trial using reflex-dom.

---
+++ app/src/StandOffApp/Form.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module StandOffApp.Form
+  where
+
+import Reflex.Dom
+import qualified Data.Text as T
+import qualified Data.Map as Map
+import           Data.Monoid((<>))
+
+
+-- labelWidget :: forall k t m a. (DomBuilder t m, MonadFix m, MonadHold t m, PostBuild t m)
+--                => T.Text
+--             -> T.Text
+--             -> k -> m a
+labelWidget label xid widget = do
+  el "div" $ do
+    el "div" $ text label
+    widget
