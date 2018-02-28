8552fc0af5eecb486fc4820636d4f82864ae901e
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-03-01 00:19:15 +0100

    Getting bibliography fields out of dynamic list of widgets

--- app/src/StandOffApp/Bibliography.hs
+++ app/src/StandOffApp/Bibliography.hs
@@ -2,15 +2,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE FlexibleContexts #-}
 module StandOffApp.Bibliography
   where
 
-import Reflex.Dom
+import Reflex.Dom hiding (element)
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import           Data.Monoid((<>))
-import           Data.Maybe (fromJust)
+import           Data.Maybe --(fromJust)
 import Control.Monad
+import Control.Lens
+
 
 import StandOffApp.DomUtils
 
@@ -23,6 +26,8 @@ bibInputWizard = el "div" $ do
          textInput $ def & attributes .~ constDyn ("placeholder" =: "Key") 
   typ <- labelWidget "Entry Type" "bibInputWizard.entryType" $
          dropdown "book" (constDyn entryTypes) def
+  fields <- labelWidget "Entry Fields" "bibInputWizard.entryFields" $ bibInputFields def
+  -- live output
   let selTyp = entryType <$> value typ
   el "div" $ do
     text "You selected '"
@@ -30,7 +35,6 @@ bibInputWizard = el "div" $ do
     text "' to be of type '"
     dynText selTyp
     text "'."
-  bibInputFields
 
 -- | A dynamically changing set of widgets
 
@@ -44,38 +48,49 @@ bibInputWizard = el "div" $ do
 -- a) makes logic complicated. How to handle same field type? Making
 -- same field type impossible would be possible but complicated. Would
 -- that be user-friendly?
-bibInputFields :: MonadWidget t m => m ()
-bibInputFields = do
-  -- Create the + button for adding a new field.
-  addNewField :: Event t () <- button "+"
-  -- Number of clicks of the button.
-  latestId :: Dynamic t Int <- count addNewField
-  -- On each addNewField event add a new field with Just "blank" field
-  -- type.
-  let modifyChildren :: Event t (Map.Map Int (Maybe ()))
-      modifyChildren = fmap (\n -> n =: Just ()) $ updated latestId
-  -- Draw the list of elements.
-  fields :: Dynamic t (Map.Map Int (Dynamic t T.Text,  Dynamic t T.Text)) <- listHoldWithKey mempty modifyChildren $ \n _ -> el "div" $ do
-    text $ T.pack $ show n
-    -- Draw a single textbox, and return its current value as a Dynamic t Text
-    bibFieldInput "blank"
-    
-  -- Combine the inner Dynamics into the outer one
-  -- let combinedValues :: Dynamic t (Map.Map Int (Dynamic t T.Text, Dynamic t T.Text))
-  --     combinedValues = joinDynThroughMap fields
-  -- -- Show the results as text
-  -- display combinedValues
-  el "br" blank
-  
+bibInputFields :: MonadWidget t m => TextInputConfig t -> m ()
+--bibInputFields :: (DomBuilder t m, PostBuild t m, DomBuilderSpace m ~ GhcjsDomSpace) => TextInputConfig t -> m ([(T.Text, T.Text)] t)
+bibInputFields (TextInputConfig inputType initial eSetValue dAttrs) = do
+  let entryFieldsMap = Map.fromList entryFields
+  rec
+    -- starting at -1 to get index on entryFields right
+    fldCnt :: Dynamic t Int <- foldDyn (+) (-1 :: Int) (1 <$ evIncrFldCnt)
+    -- On each addNewField event add a new field with Just "blank"
+    -- field type.
+    let -- modifyChildren :: Event t (Map.Map Int (Maybe ()))
+        modifyChildren = fmap (\n -> n =: Just ()) $ updated fldCnt
+    -- Draw the dynamic collection of widgets.
+    rows :: Dynamic t (Map.Map Int (Dynamic t T.Text, Dynamic t T.Text, Dynamic t Int)) <-
+      listHoldWithKey mempty modifyChildren $ \n _ -> el "div" $ do
+      -- Draw a row of dropdown, text input and deletion button.
+      let k = fromMaybe "unkown" (fmap fst (entryFields ^? element n))
+      el "div" $ do
+        fld <- dropdown k (constDyn entryFieldsMap) def
+        val <- textInput def -- $
+               --def & attributes .~ constDyn ("placeholder" =: "field value")
+        evDel <- button "-"
+        text $ T.pack $ show n
+        delFldN :: Dynamic t Int <- foldDyn (*) (1 :: Int) (n <$ evDel)
+        return ((value fld), (value val), (n <$ delFldN))
+    -- Draw button to add a new row.
+    evIncrFldCnt <- button "+"
+    -- get deletion events from field rows
+    rowDels :: Dynamic t [Dynamic t Int] <-
+      return $ fmap (Map.foldr (\(_, _, d) acc -> d : acc) []) rows
+    fields :: Dynamic t [(T.Text, T.Text)] <-
+      -- howto lift [] into Dynamic t for (liftM2 (:) (liftM2 (,) k v) acc)?
+      return $ join $ fmap (sequence . (Map.foldr (\(k, v, _) acc -> (liftM2 (,) k v) : acc) [])) rows
+  return ()
 
-bibFieldInput k = do
+
+bibFieldInput k n fldsMap = do
   el "div" $ do
-    fld <- dropdown k (constDyn entryFields) def
+    text $ T.pack $ show n
+    fld <- dropdown k (constDyn fldsMap) def
     val <- textInput def -- $
            --def & attributes .~ constDyn ("placeholder" =: "field value")
-    let _fld = entryField <$> value fld
-        _val = _textInput_value val
-    return (_fld, _val)
+    evDel <- button "-"
+    return ((value fld), (value val), (n <$ evDel))
 
 
 entryTypes :: Map.Map T.Text T.Text
@@ -84,8 +99,5 @@ entryTypes = Map.fromList [("book", "Book"), ("article", "Article"), ("inproceed
 entryType :: T.Text -> T.Text
 entryType key = fromJust (Map.lookup key entryTypes)
 
-entryFields :: Map.Map T.Text T.Text
-entryFields = Map.fromList [("author", "Author"), ("title", "Title"), ("location" , "Location"), ("year", "Year")]
-
-entryField :: T.Text -> T.Text
-entryField key = fromJust (Map.lookup key entryFields)
+entryFields :: [(T.Text, T.Text)]
+entryFields = [("author", "Author"), ("title", "Title"), ("location" , "Location"), ("year", "Year")]
