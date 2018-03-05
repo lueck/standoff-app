cbaadaf63916409c0561db034478afdaafb633f4
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-03-05 21:58:52 +0100

    Bibliography widget based on DynamicList

--- app/src/StandOffApp/Bibliography/Widget.hs
+++ app/src/StandOffApp/Bibliography/Widget.hs
@@ -22,6 +22,7 @@ import StandOffApp.DomUtils
 import StandOffApp.Bibliography.Xhr
 import StandOffApp.Bibliography.TypeDefs
 import StandOffApp.Bibliography.Bibtex
+import StandOffApp.DynamicList
 
 
 --bibInputWizard :: MonadWidget t m => m ()
@@ -33,7 +34,66 @@ bibInputWizard :: ( DomBuilder t m
                   )
                   => m ()
 bibInputWizard = el "div" $ do
+  let initFlds = []
+  rec
+    el "h2" $ text "Create New Bibliography Entry"
+    key <- labelWidget "Entry Key" "bibInputWizard.entryKey" $
+           textInput $ def & attributes .~ constDyn ("placeholder" =: "Key")
+    typ <- labelWidget "Entry Type" "bibInputWizard.entryType" $
+           dropdown "book" (constDyn entryTypes) def
+
+    -- Fields
+    fields :: Dynamic t [(T.Text, T.Text)] <-
+      labelWidget "Entry Fields" "bibInputWizard.entryFields" $ do
+      rec
+        fldCnt :: Dynamic t Int <-
+          foldDyn (+) (-1 :: Int) (1 <$ evIncrFldCnt)
+        rows :: Dynamic t [(Dynamic t T.Text, Dynamic t T.Text, Event t ())] <-
+          dynamicList fieldWidget extractRemoveEv (const never) (addRow fldCnt) initFlds
+        evIncrFldCnt :: Event t () <-
+          button "+"
+      return $ join $ fmap (sequence . (map (\(k, v, _) -> (liftM2 (,) k v)))) rows
+      
+    let entry :: Dynamic t Entry =
+          liftM3 Entry (value typ) (value key) fields
+
+    -- live output
+    --el "div" $ do
+    --  bibtexEntry entry
+  return ()
+  where
+    entryFieldsMap = Map.fromList entryFields
+    fieldWidget :: Int
+                -> (T.Text, T.Text)
+                -> Event t (T.Text, T.Text)
+                -> Dynamic t (Dynamic t T.Text, Dynamic t T.Text, Event t ()) 
+    fieldWidget n initVal changedVal = el "div" $ do
+      --text $ T.pack $ show n
+      fld <- dropdown (fieldKey n) (constDyn entryFieldsMap) def
+      val <- textInput def -- $
+             --def & attributes .~ constDyn ("placeholder" =: "field value")
+      evDel <- button "-"
+      --delFldN :: Dynamic t Int <- foldDyn (*) (1 :: Int) (n <$ evDel)
+      return ((value fld), (value val), evDel)
+    extractRemoveEv :: (Dynamic t T.Text, Dynamic t T.Text, Event t ())
+                    -> Event t ()
+    extractRemoveEv = (\(_, _, e) -> e)
+    addRow :: Dynamic t Int -> Event t (T.Text, T.Text)
+    addRow cnt = fmap (\n -> (fieldKey n, "")) $ updated cnt
+    fieldKey :: Int -> T.Text
+    fieldKey n = fromMaybe "unkown" (fmap fst (entryFields ^? element n))
+
+--bibInputWizard :: MonadWidget t m => m ()
+bibInputWizardOFF :: ( DomBuilder t m
+                  , DomBuilderSpace m ~ GhcjsDomSpace
+                  , MonadFix m
+                  , MonadHold t m
+                  , PostBuild t m
+                  )
+                  => m ()
+bibInputWizardOFF = el "div" $ do
   let entryFieldsMap = Map.fromList entryFields
+  let initFlds = []
   rec
     el "h2" $ text "Create New Bibliography Entry"
     key <- labelWidget "Entry Key" "bibInputWizard.entryKey" $
