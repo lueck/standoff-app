aba33732d272b30fa81c70d385bd84952cf7766f
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-03-18 22:11:10 +0100

    Removed language extensions not required.

--- app/src/StandOffApp/Bibliography/Format.hs
+++ app/src/StandOffApp/Bibliography/Format.hs
@@ -1,7 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MonoLocalBinds #-}
 module StandOffApp.Bibliography.Format
   where
 
