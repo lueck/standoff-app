585a50d5e7625245c888659016b3605107bb0671
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-03-19 01:47:01 +0100

    login returns token.

--- app/src/StandOffApp/Auth/Widget.hs
+++ app/src/StandOffApp/Auth/Widget.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module StandOffApp.Auth.Widget
   where
 
@@ -9,12 +10,14 @@ import qualified Data.Map as Map
 import           Data.Maybe --(fromJust)
 import Control.Monad
 import Data.Monoid ((<>))
+import Data.Aeson (decode)
+
 
 import StandOffApp.Config
 import StandOffApp.DomUtils
 
 
-loginWidget :: MonadWidget t m => m ()
+loginWidget :: MonadWidget t m => m (Dynamic t (Maybe T.Text))
 loginWidget = el "div" $ do
   user <- labelWidget "User Name" "login.username" $
           textInput $ def & attributes .~ constDyn ("placeholder" =: "User name")
@@ -22,17 +25,37 @@ loginWidget = el "div" $ do
          textInput $ def & attributes .~ constDyn ("placeholder" =: "Password")
   evLogin <- button "Login"
   -- See https://obsidian.systems/reflex-nyhug/#/step-26
-  let loginData = tag (current (liftM2 (,) (value user) (value pwd))) evLogin 
-  evRsp <- performRequestAsync $ uncurry buildReq <$> loginData
-  let evResult = (fromMaybe "" . _xhrResponse_responseText) <$> evRsp
-  el "h5" $ text "Response:"
-  dynText =<< holdDyn "" evResult
+  let loginData = tag (current (liftM2 (,) (value user) (value pwd))) evLogin
+  evRsp <- performRequestAsyncWithError $ uncurry buildReq <$> loginData
+  let evToken = -- :: Event t (Maybe T.Text) =
+        (fmap (either
+               (const Nothing) -- when the request fails
+                (\r -> fmap T.pack $ -- convert String to Text
+                       join $  -- Just Nothing -> Nothing etc.
+                       fmap
+                       (Map.lookup "token") -- get the token
+                       -- parse the response to a map
+                       ((decodeXhrResponse r) :: Maybe (Map.Map String String))))
+         evRsp)
+  token :: Dynamic t (Maybe T.Text) <- holdDyn Nothing evToken
+
+  -- print the response
+  let evResult = (either (T.pack . show) (fromMaybe "" . _xhrResponse_responseText)) <$> evRsp
+  el "div" $ do
+    text "Response:"
+    el "br" blank
+    dynText =<< holdDyn "" evResult
+  el "div" $ do
+    text "Your Authentication Token: "
+    el "br" blank
+    dynText $ fmap (fromMaybe "") token
+  
+  -- return the token
+  return token
   where
     buildReq usr pwd = XhrRequest "POST" (configUri _cfg_loginPath cfg) rqCfg
       where
         rqCfg = def
-                -- & xhrRequestConfig_user .~ (Just usr)
-                -- & xhrRequestConfig_password .~ (Just pwd)
                 & xhrRequestConfig_sendData .~ (credJson usr pwd)
                 & xhrRequestConfig_headers .~ Map.fromList [("Content-Type", "application/json")]
     credJson usr pwd = "{ \"login\": \""
